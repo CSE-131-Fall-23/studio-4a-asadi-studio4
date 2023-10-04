847cad017f3cb1841eafc7f9f4049a6eaa5db638
leila1 <leila1@localhost>
2023-10-04 15:41:23 -0500

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
-	}
+		StdDraw.setPenColor(241, 91, 134);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2);
+		double[] x = {0.1, 0.2, 0.3, 0.2};
+		double[] y = {0.6, 0.7, 0.6, 0.5};
+		StdDraw.setPenColor(251, 241, 134);
+		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenColor(225, 0, 0);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		double[] xt = {0.5, 0.55, 0.5,};
+		double[] yt = {0.55, 0.6, 0.65,};
+		StdDraw.setPenColor(44, 251, 28);
+		StdDraw.filledPolygon(xt, yt);
+		}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javax.sound.sampled.AudioFileFormat.Type;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
@@ -20,5 +21,19 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String type = in.next();
+		if(type.equals("rectangle")) {
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		StdDraw.setPenColor(r, g, b);
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
 	}
+}
 }
